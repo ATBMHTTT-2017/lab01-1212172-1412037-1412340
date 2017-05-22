2bb472227c63e81602171f548d74fd5799d27b21
khanhphan1212172 <ngonluatuoitre@gmail.com>
2017-05-22 08:30:35 +0700

    Thêm phần nhập liệu và bổ sung role rGiamDoc

---
+++ DAC_TruongPhong_insert-update_DuAn.sql
@@ -0,0 +1,6 @@
+--Chi truong phong duoc phep cap nhat va them thong tin vao du an(DAC)
+--
+GRANT insert, update ON SYS.NhanVien TO nv01
+
+--Neu su dung RBAC thi se ngan gon hon nhieu
+CRANT INSERT , UPDATE ON SYS.DuAn  TO rTruongPhong ;
