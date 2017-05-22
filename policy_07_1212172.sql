88600a31c8a0f2d6697ea313154dd849e8bc553b
khanhphan1212172 <ngonluatuoitre@gmail.com>
2017-05-22 09:42:30 +0700

    policy_07
    truong du an chi duoc phep doc ghi thong tin chi tieu cua du an minh
    quan li

---
+++ policy_07_1212172.sql
@@ -0,0 +1,31 @@
+--truong du an chi duoc phep doc ghi thong tin chi tieu cua du an minh quan li
+
+CREATE OR REPLACE FUNCTION VPD_TruongDuAn_ChiTieu (p_schema VARCHAR2, p_obj VARCHAR2)
+RETURN VARCHAR2
+AS
+  user VARCHAR2(100);
+  ma_DA VARCHAR2(10);
+  n_b NUMBER;
+BEGIN
+  user:= SYS_CONTEXT('userenv','SESSION_USER');
+  SELECT COUNT(*) INTO n_b FROM SYS.PhongBan WHERE user = truongPhong;
+  IF(n_b > 0) THEN
+    RETURN '';
+  ELSE
+    BEGIN
+     SELECT maDA INTO ma_DA FROM SYS.DuAn WHERE truongDA = user;
+     RETURN 'duAn='|| q'[']' || ma_DA || q'[']';
+    END;
+  END IF;
+  EXCEPTION
+      WHEN OTHERS THEN RETURN '1 = 0';
+END;
+
+--them policy vao bang du lieu ChiTieu
+EXECUTE dbms_rls.add_policy(object_schema=>'SYS',
+                        object_name=>'SYS.ChiTieu',
+                        policy_name=>'VPD_TruongDuAn_ChiTieu',
+                        function_schema=>'SYS',
+                        policy_function=>'VPD_TruongDuAn_ChiTieu',
+                        statement_types=>'select,update,insert',
+                        update_check=>TRUE);
